548b664b4d610ce49ed111fe28447cf902ceed12
chidi <dhammondg@gmail.com>
2024-06-02 12:50:21 -0500

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ create a repository as container in the github
 name the repository
 make sure the name does not exist
 insert the name which is unique
+you should be able to push codes to github
+thererfore create an account in github
+then you are good to go
 
