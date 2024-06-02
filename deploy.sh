e241b4210197df567804867382580c7d2eaaa020
Gabiham1977 <dhammondg@mail.com>
2024-06-02 14:20:37 +0000

    deploy created

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+This application is due to be processed
+The following are the steps to follow:
+write a script 
+commit the script to git/ github
+create a repository as container in the github
+name the repository
+make sure the name does not exist
+
